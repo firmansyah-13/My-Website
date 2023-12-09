5d51089c00645abface17519ee27e7df713a903e
Firmansyah-13 <firman130305@gmail.com>
2023-12-09 21:15:49 +0700

    membuat navbar

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+// Nvabar fixed
+window.onscroll = function() {
+    
+}
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+hamburger.addEventListener('click', function(){
+    console.log('tes');
+    hamburger.classList.toggle('hamburger-active');
+    
+})
