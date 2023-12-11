7631f8c3e1cab0603bb2b71124d35842090a77b2
Firmansyah-13 <firman130305@gmail.com>
2023-12-11 09:08:18 +0700

    Portfolio section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,26 @@
 // Nvabar fixed
 window.onscroll = function() {
-    
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.scrollY > fixedNav){
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
 }
+
+
 // hamburger
+const navMenu = document.querySelector('#nav-menu');
 const hamburger = document.querySelector('#hamburger');
+
 hamburger.addEventListener('click', function(){
-    console.log('tes');
     hamburger.classList.toggle('hamburger-active');
-    
+    navMenu.classList.toggle('hidden');
+})
+
+navMenu.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 })
